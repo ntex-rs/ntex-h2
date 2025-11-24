bca3a3b1806045dcf2ab216c867306ae925b471a
Nikolay Kim <fafhrd91@gmail.com>
2025-11-24 10:26:01 +0100

    Bump version

--- src/hpack/table.rs
+++ src/hpack/table.rs
@@ -1,6 +1,6 @@
-use std::{cmp, collections::VecDeque, hash::Hash, hash::Hasher, mem};
+use std::{cmp, collections::VecDeque, hash::BuildHasher, hash::Hash, hash::Hasher, mem};
 
-use ahash::AHasher;
+use foldhash::fast::RandomState;
 use ntex_http::{header, Method};
 
 use super::Header;
@@ -661,7 +661,7 @@ fn probe_distance(mask: usize, hash: HashValue, current: usize) -> usize {
 fn hash_header(header: &Header) -> HashValue {
     const MASK: u64 = (MAX_SIZE as u64) - 1;
 
-    let mut h = AHasher::default();
+    let mut h = RandomState::default().build_hasher();
     header.name().hash(&mut h);
     HashValue((h.finish() & MASK) as usize)
 }
