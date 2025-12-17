972aeaf5fbba736e965fe04f736376e39f330fce
Nikolay Kim <fafhrd91@gmail.com>
2025-12-17 15:29:49 +0100

    Upgrade to Service v4 (#73)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changes
 
+## [3.2.0] - 2025-12-17
+
+* Upgrade to ntex-service v4
+
 ## [3.1.0] - 2025-12-04
 
 * Refactor stream ID validation and error handling #71
--- src/hpack/table.rs
+++ src/hpack/table.rs
@@ -1,4 +1,4 @@
-use std::{cmp, collections::VecDeque, hash::Hash, hash::Hasher, mem};
+use std::{cmp, collections::VecDeque, hash::BuildHasher, hash::Hash, hash::Hasher, mem};
 
 use ntex_http::{Method, header};
 
@@ -660,7 +660,7 @@ fn probe_distance(mask: usize, hash: HashValue, current: usize) -> usize {
 fn hash_header(header: &Header) -> HashValue {
     const MASK: u64 = (MAX_SIZE as u64) - 1;
 
-    let mut h = ahash::AHasher::default();
+    let mut h = foldhash::fast::FixedState::default().build_hasher();
     header.name().hash(&mut h);
     HashValue((h.finish() & MASK) as usize)
 }
